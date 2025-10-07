7c69dd4cfbbe361baed347df0d429f929d0c170a
Wendy GERVAIS <wendy.gervais@reciproque.com>
2025-10-07 18:38:23 +0200

    choose lang. texts from json + begin select

--- src/components/LanguageScreen.vue
+++ src/components/LanguageScreen.vue
@@ -7,16 +7,33 @@ defineProps({
   },
 })
 
+import texts from '../../assets/interface.json'
+
 import { gsap } from 'gsap';
 
-import { onMounted } from 'vue'
+import { onMounted } from 'vue';
+
+function getText(n, lang) {
+  if (lang=="FR") return texts[n]["texte-FR"];
+  if (lang=="EN") return texts[n]["texte-EN"];
+  if (lang=="DE") return texts[n]["texte-DE"];
+}
+
+let lang = "FR";
+
+function select(selectedLang) {
+  lang = selectedLang;
+  gsap.from(document.getElementById(selectedLang), {scale:0.9, duration:1, ease:"bounce.out"})
+} 
 
 onMounted(() => {
-gsap.from(document.querySelector("h1"), {opacity:0, duration:3})
-gsap.from(document.querySelector(".paraph-lang"), {opacity:0, duration:3, delay:1})
-gsap.from(document.getElementById("fr"), {opacity:0, duration:4, delay:0.5})
-gsap.from(document.getElementById("en"), {opacity:0, duration:4, delay:0.75})
-gsap.from(document.getElementById("de"), {opacity:0, duration:4, delay:1})
+  gsap.from(document.querySelector("h1"), {opacity:0, duration:3})
+  gsap.from(document.querySelectorAll("h2"), {opacity:0, duration:3})
+
+  gsap.from(document.querySelector(".paraph-lang"), {opacity:0, duration:3, delay:1})
+  gsap.from(document.getElementById("FR"), {opacity:0, duration:4, delay:0.5})
+  gsap.from(document.getElementById("EN"), {opacity:0, duration:4, delay:0.75})
+  gsap.from(document.getElementById("DE"), {opacity:0, duration:4, delay:1})
 
 })
 
@@ -25,17 +42,17 @@ gsap.from(document.getElementById("de"), {opacity:0, duration:4, delay:1})
 
 <template>
   <div class="language-screen">
-    <!-- TODO : Textes interface depuis JSON. -->
-  <h1>Le métier du figuriniste</h1> 
+  <h1>{{ getText(0, lang) }}</h1> <br>
+  <h2>{{ getText(1, lang) }}</h2> <br>
+  <h2>{{ getText(2, lang) }}</h2> 
   <div class="flags-row">
-    <div class="flag" id="fr"><img src="../../assets/fr.png" alt=""></div>
-    <div class="flag" id="en"><img src="../../assets/en.png" alt=""></div>
-    <div class="flag" id="de"><img src="../../assets/de.png" alt=""></div>
+    <div class="flag" id="FR"><img src="../../assets/fr.png" alt="" @click="select('FR')"></div>
+    <div class="flag" id="EN"><img src="../../assets/en.png" alt="" @click="select('EN')"></div>
+    <div class="flag" id="DE"><img src="../../assets/de.png" alt="" @click="select('DE')"></div>
   </div>
-    <!-- TODO : Textes interface depuis JSON. -->
-  <div class="paraph-lang"><span>Choisissez votre langue</span></br>
-    <span>Choose your language</span> </br>
-    <span>Wählen Sie Ihre Sprache</span> </br>
+  <div class="paraph-lang"><span>{{ getText(3, lang) }}</span></br>
+    <span>{{ getText(4, lang) }}</span> </br>
+    <span>{{ getText(5, lang) }}</span> </br>
   </div></div>
 
 </template>
@@ -55,6 +72,14 @@ h1 {
   font-weight: 800;
   padding-top: 180px;
 
+}
+
+
+h2 {
+  font-size: 50px;
+  font-family: 'Gotham-Black';
+  text-transform: uppercase;
+  font-weight: 800;
 }
 
 .flag {
