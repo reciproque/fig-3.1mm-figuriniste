fd73b4b0a24b2a97fbeb793bd903e69d06069f4a
Wendy Gervais <wendy.gervais@reciproque.com>
2025-11-19 18:19:04 +0100

    non looping peinture gifs

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import VideoPlayer from './components/VideoPlayer.vue'
 const dialogs = ref({});
 const texts = ref({});
 
-let selectedStartId = 67;
+let selectedStartId = 62;
 
 onMounted(async () => {
   try {
@@ -48,7 +48,7 @@ function onLanguageSelected(lang) {
     <div class="debug debug-versions"
       v-if="dialogs && Object.keys(dialogs).length && texts && Object.keys(texts).length">
       Numéro de versions<br>
-      Build DEV du 14.11.25 à 15h30<br>
+      Build DEV du 19.11.25 à 15h45<br>
       Interface : {{ texts[Object.keys(texts).length - 1]["texte-FR"] }} <br>
       Dialogues : {{ dialogs[Object.keys(dialogs).length - 1]["texte-FR"] }} <br>
 
